43dcebcc02ecbb6a23f0962d5516fb5551aa6e50
SlemJosh <joshua.slem@gmail.com>
2024-01-17 14:07:03 -0700

    Added rules for modules and comments

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,15 @@
-const HtmlWebpackPlug,in = require('html-webpack-plugin');
+// Import necessary modules and plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
+    // Setting the mode to development for more readable output and easier debugging
     mode: 'development',
+
+    // Entry points for various JavaScript files
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
@@ -16,47 +17,63 @@ module.exports = () => {
       editor: './src/js/editor.js',
       header: './src/js/header.js',
     },
+
+    // Output configuration
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js', // Naming convention for output files
+      path: path.resolve(__dirname, 'dist'), // Output directory
     },
+
+    // Plugins used in this configuration
     plugins: [
-      // Webpack plugin
+      // Generates an HTML file from a template and injects scripts
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE'
-      }),
-      
-      // service worker 
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        template: './index.html', // Source template file
+        title: 'JATE' // Title of the application
       }),
 
-      // WebpackPwa Manifest
+      // Configures the service worker using Workbox's InjectManifest plugin
+      new InjectManifest({
+        swSrc: './src-sw.js', // Source service worker file
+        swDest: 'src-sw.js', // Destination for the injected service worker
+      }),
+
+      // Generates a manifest file for the PWA
       new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'JATE', 
-        description: 'Just Another Text Editor',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
-        icons: [
+        fingerprints: false, // Disables fingerprints in filenames
+        inject: true, // Automatically injects the manifest into the HTML
+        name: 'Just Another Text Editor', // Name of the PWA
+        short_name: 'JATE', // Short name of the PWA
+        description: 'Just Another Text Editor', // Description of the PWA
+        background_color: '#225ca3', // Background color
+        theme_color: '#225ca3', // Theme color
+        start_url: './', // Start URL when launched
+        publicPath: './', // Public path for assets
+        icons: [ // Array of icons for different resolutions
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            sizes: [96, 128, 192, 256, 384, 512], // Icon sizes
+            destination: path.join('assets', 'icons'), // Destination for icons
           }
         ]
       })
     ],
 
+    // Module rules for handling different file types
     module: {
       rules: [
-        
+        // Rule for CSS files
+        {
+          test: /\.css\$/i, // Regex to match CSS files
+          exclude: /node_modules/, // Excluding node_modules
+          use: {
+            loader: 'babel-loader', // Using Babel loader for transpiling
+            options: {
+              presets: ['@babel/preset-env'], // Preset for latest JavaScript features
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
