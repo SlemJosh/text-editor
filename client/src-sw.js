ff47dc89c6ffe891b208a4d5a704f78879cd5a5b
SlemJosh <joshua.slem@gmail.com>
2024-01-17 17:09:01 -0700

    Cleaned up code

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,4 +1,4 @@
-// Importing Workbox recipes and strategies
+// Workbox imports
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -6,46 +6,26 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-// Precaching assets: Automatically caches important files during service worker installation
+// Precaching important files during service worker installation
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Page caching strategy: Caches pages and uses them for future navigational requests
+// Caching strategy for pages
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache', // Naming the cache for pages
+  cacheName: 'page-cache',
   plugins: [
-    new CacheableResponsePlugin({ // Ensures that only requests with certain status codes are cached
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({ // Sets expiration for cached entries
-      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days in seconds
-    }),
+    new CacheableResponsePlugin({ statuses: [0, 200] }),
+    new ExpirationPlugin({ maxAgeSeconds: 30 * 24 * 60 * 60 }),
   ],
 });
 
-// Warming strategy cache: Preloads and caches specified URLs
-warmStrategyCache({
-  urls: ['/index.html', '/'], // URLs to preload and cache
-  strategy: pageCache, // Using pageCache strategy defined above
-});
+// Cache strategy for navigation requests
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// Register route for navigation requests: Uses pageCache strategy for pages navigation
+// Cache strategy for assets (styles, scripts, workers)
 registerRoute(
-  ({ request }) => request.mode === 'navigate', // Condition to match navigation requests
-  pageCache // Using the pageCache strategy for matched requests
-);
-
-// Register route for static assets: Caches different types of assets
-registerRoute(
-  // Matching criteria for assets
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  
-  // StaleWhileRevalidate strategy: Serves cached content while updating the cache in the background
   new StaleWhileRevalidate({
-    cacheName: 'asset-cache', // Naming the cache for assets
-    plugins: [
-      new CacheableResponsePlugin({ // Ensures that only requests with certain status codes are cached
-        statuses: [0, 200],
-      }),
-    ],
+    cacheName: 'asset-cache',
+    plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })],
   })
 );
